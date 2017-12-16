25e517aa2b8ed3f46ebae5887771ed99fe21acd8
moty pitusy <moty.p2c@gmail.com>
2017-12-16 19:18:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var height = "74"
 if (name==="suzan"){
   consol.log("The name is correct");
 }
+else {
+  consol.log("Expected the name to be Suzan")
+}
 
 // Don't worry about this
 module.exports = { name, height
